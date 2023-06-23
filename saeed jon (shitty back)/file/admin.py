5481adc3d218c9710369b41db8d23c7b9de9c45b
MinusMarigold98 <starehsohili@gmail.com>
2023-06-23 16:20:16 +0330

    remove shitty python backend

--- saeed jon (shitty back)/file/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import HomeWork
-# Register your models here.
-
-@admin.register(HomeWork)
-class HomeWorkAdmin(admin.ModelAdmin):
-    list_display = ('user', 'title', 'status')
